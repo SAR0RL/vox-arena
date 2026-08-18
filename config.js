997e4fd17775dad25665ae67e5c357f09cc20f4f
SAR0RL <trinten245@gmail.com>
2026-08-17 20:57:54 -0500

    Publish VOX Arena

---
+++ config.js
@@ -0,0 +1,5 @@
+// Add your Supabase Project URL and Publishable/Anon key here before deploying.
+window.VOX_CONFIG = {
+  supabaseUrl: 'https://kdayzdtrhtphidkmodnu.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Z8fLvJ5aAS1RIryEVyWqUA_Vp3tuk8O'
+};
